1d572cbdc0fd5e143960e47a0a8e7ba543ffa951
IgnatyevPavel <99251507+IgnatyevPavel@users.noreply.github.com>
2024-02-02 13:10:21 +0500

    update

---
+++ js practice/intro.js
@@ -0,0 +1,39 @@
+console.log('Hello');
+
+var yolochka1 = 100;
+var n2 = 50;
+var result = yolochka1 + n2;
+
+console.log(result);
+
+var n3 = 50
+console.log(n2 - n3 * yolochka1);
+console.log("----------------------------------разделитель------------------------------------");
+
+var salary = 1000;
+var month = 6;
+var salary_6_month = salary * month; 
+var u_name = "Pavel";
+var hi_message = 'Hello,'
+var salary_message = 'your 6 mounth salary is '
+var currency = ' USDT'
+
+console.log(hi_message, u_name, salary_message, salary_6_month);
+console.log("----------------------------------разделитель------------------------------------");
+console.log("Hello, Pavel your 6 mounth salary is", salary_6_month, currency);
+console.log("----------------------------------разделитель------------------------------------");
+console.log(hi_message + u_name + salary_message + salary_6_month + currency);
+console.log("----------------------------------разделитель------------------------------------");
+
+var b1 = true;
+var b2 = false;
+console.log (b1);
+console.log('значение переменной b1: ', b1, ' Тип переменной: ',typeof + b1);
+console.log('значение переменной u_name: ', u_name, ' Тип переменной: ', typeof u_name);
+console.log('значение переменной salary: ', salary, ' Тип переменной: ', typeof salary);
+console.log("----------------------------------разделитель------------------------------------");
+
+let num_33 = +'33';
+let num_3 = '33';
+console.log(typeof num_33)
+console.log(typeof num_3)
